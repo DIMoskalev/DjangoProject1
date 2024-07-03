c8ac4366cbf79eef888dfa9b4f8ed82bb4eb7bf3
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-07-03 21:22:05 +0300

    add methods get_context_data and from_valid for class StudentUpdateView

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 
-from main.forms import StudentForm
-from main.models import Student
+from main.forms import StudentForm, SubjectForm
+from main.models import Student, Subject
 
 
 class StudentListView(ListView):
@@ -37,6 +38,24 @@ class StudentUpdateView(UpdateView):
     model = Student
     form_class = StudentForm
     success_url = reverse_lazy('main:base')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        SubjectFormset = inlineformset_factory(Student, Subject, form=SubjectForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = SubjectFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = SubjectFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
 
 
 class StudentDeleteView(DeleteView):
